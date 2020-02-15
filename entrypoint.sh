69a0131a071d0bb5725791444475a3eb69f308b1
Seungyong Yun <devussy88@gmail.com>
2020-02-15 22:28:04 +0900

    Update distribute logic

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,3 +1,15 @@
 #!/bin/sh
-appcenter distribute release --token "$INPUT_TOKEN" --app "$INPUT_APPNAME" --group "$INPUT_GROUP" --file "$INPUT_FILE" --release-notes "$INPUT_RELEASENOTES" --debug
+
+is_silent=$INPUT_SILENT
+if [ $is_silent = true ] ; then
+  appcenter distribute release --token "$INPUT_TOKEN" --app "$INPUT_APP" --group "Collaborators" --file "$INPUT_FILE" --release-notes "$INPUT_RELEASENOTES" --debug --silent
+
+  latest_version="$(appcenter distribute releases list --token "$INPUT_TOKEN" --app "$INPUT_APP"  | grep ID | head -1| tr -s ' ' | cut -f2 -d ' ')"
+  appcenter distribute releases add-destination --token "$INPUT_TOKEN" --app "$INPUT_APP" --destination "$INPUT_GROUP" -t group -r "$latest_version" --silent
+else
+  appcenter distribute release --token "$INPUT_TOKEN" --app "$INPUT_APP" --group "Collaborators" --file "$INPUT_FILE" --release-notes "$INPUT_RELEASENOTES" --debug
+
+  latest_version="$(appcenter distribute releases list --token "$INPUT_TOKEN" --app "$INPUT_APP"  | grep ID | head -1| tr -s ' ' | cut -f2 -d ' ')"
+  appcenter distribute releases add-destination --token "$INPUT_TOKEN" --app "$INPUT_APP" --destination "$INPUT_GROUP" -t group -r "$latest_version"
+fi
 
